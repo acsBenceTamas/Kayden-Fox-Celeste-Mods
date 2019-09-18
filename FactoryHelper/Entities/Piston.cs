a9d0851ff3f935c07256b6b5d83c2da9bed7d397
Acsb <Acsb@ACSB>
2019-09-18 11:27:39 +0200

    Up facing Piston works

--- FactoryHelper/Entities/Piston.cs
+++ FactoryHelper/Entities/Piston.cs
@@ -2,34 +2,104 @@
 using Microsoft.Xna.Framework;
 using Monocle;
 using System;
+using System.Collections;
 
 namespace FactoryHelper.Entities
 {
     class Piston : Entity
     {
-        public Piston(Vector2 position, Vector2 start, Vector2 end, string direction)
+        public float MoveTime { get; } = 0.2f;
+        public float PauseTime { get; } = 0.4f;
+
+        public float Percent { get; private set; } = 0f;
+
+        public bool MoveForward { get; private set; } = true;
+        
+        public bool Moving { get; private set; } = true;
+
+        public bool Activated { get; private set; }
+        public float PauseTimer { get; private set; }
+
+        private const int _bodyVariantCount = 4;
+        private PistonPart _head;
+        private PistonPart[] _body;
+        private PistonPart _base;
+        private Vector2 _startPos;
+        private Vector2 _endPos;
+        private Vector2 _basePos;
+        private int _bodyPartCount;
+
+        public Piston(Vector2 position, Vector2 start, Vector2 end, string directionString)
         {
+            Activated = true;
             Random rnd = new Random();
             double length;
-            int bodyPartCount;
-            //if (direction == Directions.Up || direction == Directions.Down)
-            //{
+            Directions direction = Directions.Up;
+            Enum.TryParse<Directions>(directionString, out direction);
+
+            _startPos = start;
+            _endPos = end;
+            _basePos = position;
+
+            if (direction == Directions.Up || direction == Directions.Down)
+            {
                 length = Math.Max(position.Y - end.Y, position.Y - start.Y);
-                bodyPartCount = (int) Math.Ceiling(length / 8);
+                _bodyPartCount = (int) Math.Ceiling(length / 8);
 
-                _base = new PistonPart(position + new Vector2(2, 0), 12, 8, -2, 0, "objects/FactoryHelper/piston/base");
-                _base.Depth = -10;
-                _head = new PistonPart(end, 16, 8, 0, 0, "objects/FactoryHelper/piston/head");
-                _head.Depth = -10;
-                _body = new PistonPart[bodyPartCount];
-                for (int i = 0; i < bodyPartCount; i++)
+                _base = new PistonPart(position + new Vector2(2, 0), 12, 8, -2, 0, "objects/FactoryHelper/piston/base00");
+                _base.Depth = -20;
+                _head = new PistonPart(end, 16, 8, 0, 0, "objects/FactoryHelper/piston/head00");
+                _head.Depth = -20;
+                _body = new PistonPart[_bodyPartCount];
+                for (int i = 0; i < _bodyPartCount; i++)
                 {
-                    Vector2 offset = new Vector2(3, i * -(position.Y - end.Y) / bodyPartCount);
-                    _body[i] = new PistonPart(position + offset, 10, 8, -3, 0, "objects/FactoryHelper/piston/body");
-                    _body[i].Depth = -20;
+                    Vector2 offset = new Vector2(3, i * -(position.Y - end.Y) / _bodyPartCount);
+                    _body[i] = new PistonPart(position + offset, 10, 8, -3, 0, $"objects/FactoryHelper/piston/body0{rnd.Next(_bodyVariantCount)}");
+                    _body[i].Depth = -10;
                 }
-            //}
+            }
 
+            base.Add(new Coroutine(Sequence(), true));
+        }
+
+        private IEnumerator Sequence()
+        {
+            for (; ; )
+            {
+                while (!(Activated && Moving))
+                {
+                    yield return null;
+                }
+
+                yield return PauseTime;
+
+                while ( Percent < 1f )
+                {
+                    yield return null;
+                    Percent = Calc.Approach(Percent, 1f, Engine.DeltaTime / MoveTime);
+                    UpdatePosition();
+
+                }
+                PauseTimer = PauseTime;
+                Percent = 0f;
+                MoveForward = !MoveForward;
+            }
+        }
+
+        private void UpdatePosition()
+        {
+            var start = MoveForward ? _startPos : _endPos;
+            var end = MoveForward ? _endPos : _startPos;
+            _head.MoveTo(Vector2.Lerp(start, end, Ease.SineIn(Percent)));
+
+            for (int i = 0; i < _bodyPartCount; i++)
+            {
+                Vector2 from = new Vector2(_basePos.X + 3, i * -(_basePos.Y - _startPos.Y) / _bodyPartCount + _basePos.Y);
+                Vector2 to = new Vector2(_basePos.X + 3, i * -(_basePos.Y - _endPos.Y) / _bodyPartCount + _basePos.Y);
+                start = MoveForward ? from : to;
+                end = MoveForward ? to : from;
+                _body[i].MoveTo(Vector2.Lerp(start, end, Ease.SineIn(Percent)));
+            }
         }
 
         public override void Added(Scene scene)
@@ -58,10 +128,6 @@ namespace FactoryHelper.Entities
         {
         }
 
-        private PistonPart _head;
-        private PistonPart[] _body;
-        private PistonPart _base;
-
         public enum Directions
         {
             Up,
@@ -78,15 +144,17 @@ namespace FactoryHelper.Entities
 
             public PistonPart(Vector2 position, float width, float height, float xOffset, float yOffset, string sprite) : base(position, width, height, true)
             {
-                Sprite = new Sprite(GFX.Game, sprite);
-                // Sprite.Position = new Vector2(0 + xOffset, 0 + yOffset);
+                Sprite = new Image(GFX.Game[sprite]);
+                Sprite.Active = true;
+                Sprite.Origin = new Vector2(0, 0);
+                Sprite.Position = new Vector2(0 + xOffset, 0 + yOffset);
                 base.Add(Sprite);
                 Console.WriteLine("Adding sprite: " + sprite);
                 Console.WriteLine("Position: " + Sprite.Position);
                 Console.WriteLine("Solid Position: " + base.Position);
             }
 
-            public Sprite Sprite;
+            public Image Sprite;
 
             public override void Added(Scene scene)
             {
