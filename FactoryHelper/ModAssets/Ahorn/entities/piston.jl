4d18d507ffb7350617aa630300c25f08a0b3ac85
acsBenceTamas <acs.bence.tamas@gmail.com>
2019-09-22 16:05:00 +0200

    Mod assets

--- FactoryHelper/ModAssets/Ahorn/entities/piston.jl
+++ FactoryHelper/ModAssets/Ahorn/entities/piston.jl
@@ -2,51 +2,59 @@ module FactoryHelperPiston
 
 using ..Ahorn, Maple
 
-@mapdef Entity "FactoryHelper/Piston" Piston(x::Integer, y::Integer, moveTime::Real=0.4, pauseTime::Real=0.2, initialDelay::Real=0.0, startActive::Bool=true, activationId::String="")
+@mapdef Entity "FactoryHelper/PistonUp" PistonUp(x::Integer, y::Integer, moveTime::Real=0.4, pauseTime::Real=0.2, initialDelay::Real=0.0, startActive::Bool=true, heated::Bool=false, activationId::String="")
+@mapdef Entity "FactoryHelper/PistonDown" PistonDown(x::Integer, y::Integer, moveTime::Real=0.4, pauseTime::Real=0.2, initialDelay::Real=0.0, startActive::Bool=true, heated::Bool=false, activationId::String="")
+@mapdef Entity "FactoryHelper/PistonLeft" PistonLeft(x::Integer, y::Integer, moveTime::Real=0.4, pauseTime::Real=0.2, initialDelay::Real=0.0, startActive::Bool=true, heated::Bool=false, activationId::String="")
+@mapdef Entity "FactoryHelper/PistonRight" PistonRight(x::Integer, y::Integer, moveTime::Real=0.4, pauseTime::Real=0.2, initialDelay::Real=0.0, startActive::Bool=true, heated::Bool=false, activationId::String="")
+
+pistonUnion = Union{PistonUp, PistonDown, PistonLeft, PistonRight}
 
 const placements = Ahorn.PlacementDict(
     "Piston (Up) (FactoryHelper)" => Ahorn.EntityPlacement(
-        Piston,
+        PistonUp,
         "point",
         Dict{String, Any}(),
         function(entity)
-            entity.data["nodes"] = [(Int(entity.data["x"]), Int(entity.data["y"]) - 16), (Int(entity.data["x"]), Int(entity.data["y"]) - 32)]
-			entity.data["direction"] = "Up"
+            entity.data["nodes"] = [(Int(entity.data["x"]), Int(entity.data["y"]) - 32), (Int(entity.data["x"]), Int(entity.data["y"]) - 16)]
         end
     ),
     "Piston (Down) (FactoryHelper)" => Ahorn.EntityPlacement(
-        Piston,
+        PistonDown,
         "point",
         Dict{String, Any}(),
         function(entity)
-            entity.data["nodes"] = [(Int(entity.data["x"]), Int(entity.data["y"]) + 16), (Int(entity.data["x"]), Int(entity.data["y"]) + 32)]
-			entity.data["direction"] = "Down"
+            entity.data["nodes"] = [(Int(entity.data["x"]), Int(entity.data["y"]) + 32), (Int(entity.data["x"]), Int(entity.data["y"]) + 16)]
         end
     ),
     "Piston (Left) (FactoryHelper)" => Ahorn.EntityPlacement(
-        Piston,
+        PistonLeft,
         "point",
         Dict{String, Any}(),
         function(entity)
-            entity.data["nodes"] = [(Int(entity.data["x"]) - 16, Int(entity.data["y"])), (Int(entity.data["x"]) - 32, Int(entity.data["y"]))]
-			entity.data["direction"] = "Left"
+            entity.data["nodes"] = [(Int(entity.data["x"]) - 32, Int(entity.data["y"])), (Int(entity.data["x"]) - 16, Int(entity.data["y"]))]
         end
     ),
     "Piston (Right) (FactoryHelper)" => Ahorn.EntityPlacement(
-        Piston,
+        PistonRight,
         "point",
         Dict{String, Any}(),
         function(entity)
-            entity.data["nodes"] = [(Int(entity.data["x"]) + 16, Int(entity.data["y"])), (Int(entity.data["x"]) + 32, Int(entity.data["y"]))]
-			entity.data["direction"] = "Right"
+            entity.data["nodes"] = [(Int(entity.data["x"]) + 32, Int(entity.data["y"])), (Int(entity.data["x"]) + 16, Int(entity.data["y"]))]
         end
     ),
 )
 
-Ahorn.nodeLimits(entity::Piston) = 2, 2
+Ahorn.nodeLimits(entity::pistonUnion) = 2, 2
 	
 baseSprite = "objects/FactoryHelper/piston/base00"
 headSprite = "objects/FactoryHelper/piston/head00"
+
+directions = Dict{String, String}(
+    "FactoryHelper/PistonUp" => "Up",
+    "FactoryHelper/PistonDown" => "Down",
+    "FactoryHelper/PistonLeft" => "Left",
+    "FactoryHelper/PistonRight" => "Right",
+)
 
 directionRotation = Dict{String,Real}(
 	"Up" => 0,
@@ -62,11 +70,11 @@ directionDisplacement = Dict{String,Tuple{Integer,Integer}}(
 	"Right" => (8,0),
 )
 
-function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::Piston, room::Maple.Room)
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::pistonUnion, room::Maple.Room)
     x, y = Ahorn.position(entity)
     sx, sy = Int.(entity.data["nodes"][1])
     ex, ey = Int.(entity.data["nodes"][2])
-	direction = get(entity.data, "direction", "Up")
+	direction = get(directions, entity.name, "Up")
 	
 	y = y + 4
 	sy = sy + 4
@@ -75,45 +83,48 @@ function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::Piston, room::Ma
 	sx = sx + 8
 	ex = ex + 8
 	
+	rectangleColor1 = Tuple{Real,Real,Real,Real}
+	rectangleColor2 = Tuple{Real,Real,Real,Real}
+	
 	displaceSpriteX = directionDisplacement[direction][1]
 	displaceSpriteY = directionDisplacement[direction][2]
 	
-	if direction == "Up" || direction == "Down"
-		Ahorn.drawRectangle(ctx, x-4, ey, 8, y-ey, (0.4, 0.3, 0.2, 0.4), (0.4, 0.3, 0.2, 1.0))
-		Ahorn.drawRectangle(ctx, x-4, ey, 8, sy-ey, (0.4, 0.3, 0.2, 0.4), (0.4, 0.3, 0.2, 1.0))
+	if get(entity.data, "heated", false) == true
+		rectangleColor1 = (0.5, 0.1, 0.0, 0.4)
+		rectangleColor2 = (0.5, 0.1, 0.0, 1.0)
 	else
-		Ahorn.drawRectangle(ctx, ex-4, y, x-ex, 8, (0.4, 0.3, 0.2, 0.4), (0.4, 0.3, 0.2, 1.0))
-		Ahorn.drawRectangle(ctx, ex-4, y, sx-ex, 8, (0.4, 0.3, 0.2, 0.4), (0.4, 0.3, 0.2, 1.0))
+		rectangleColor1 = (0.4, 0.3, 0.2, 0.4)
+		rectangleColor2 = (0.4, 0.3, 0.2, 1.0)
+	end
+	
+	if direction == "Up" || direction == "Down"
+		Ahorn.drawRectangle(ctx, x-4, ey, 8, y-ey, rectangleColor1, rectangleColor2)
+		Ahorn.drawRectangle(ctx, x-4, ey, 8, sy-ey, rectangleColor1, rectangleColor2)
+	else
+		Ahorn.drawRectangle(ctx, ex-4, y, x-ex, 8, rectangleColor1, rectangleColor2)
+		Ahorn.drawRectangle(ctx, ex-4, y, sx-ex, 8, rectangleColor1, rectangleColor2)
 	end
     Ahorn.drawSprite(ctx, baseSprite, x + displaceSpriteX, y + displaceSpriteY, rot=directionRotation[direction])
     Ahorn.drawSprite(ctx, headSprite, sx + displaceSpriteX, sy + displaceSpriteY, rot=directionRotation[direction])
     Ahorn.drawSprite(ctx, headSprite, ex + displaceSpriteX, ey + displaceSpriteY, rot=directionRotation[direction])
-end
-
-
-function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::Piston)
-    sx, sy = Int.(entity.data["nodes"][1])
-    ex, ey = Int.(entity.data["nodes"][2])
-	direction = get(entity.data, "direction", "Up")
 	
-	if direction == "Up" || direction == "Down"
+	if direction == "Left" || direction == "Right"
 		sy = sy + 4
 		ey = ey + 4
-		ex = ex + 8
-		sx = sx + 8
-	else
-		sy = sy + 8
-		ey = ey + 8
-		ex = ex + 4
-		sx = sx + 4
+		ex = ex - 4
+		sx = sx - 4
 	end
-	Ahorn.drawArrow(ctx, sx, sy, ex, ey, Ahorn.colors.selection_selected_fc, headLength=6)
+	Ahorn.drawArrow(ctx, sx, sy, ex, ey, (0.0, 0.0, 0.7, 1.0), headLength=4)
 end
 
-function Ahorn.selection(entity::Piston)
+
+function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::pistonUnion)
+end
+
+function Ahorn.selection(entity::pistonUnion)
     nodes = get(entity.data, "nodes", ())
     x, y = Ahorn.position(entity)
-	direction = get(entity.data, "direction", "Up")
+	direction = get(directions, entity.name, "Up")
 	
 	y = y + 4
 	x = x + 8
---
+++ FactoryHelper/ModAssets/Ahorn/entities/windTunnel.jl
@@ -0,0 +1,75 @@
+module FactoryHelperDashFuseBox
+
+using ..Ahorn, Maple
+
+@mapdef Entity "FactoryHelper/WindTunnel" WindTunnel(x::Integer, y::Integer, width::Integer=16, height::Integer=16, direction::String="Up", activationId::String="", strength::Real=1.0, startActive::Bool=false)
+
+directions = ["Up", "Down", "Left", "Right"]
+
+const placements = Ahorn.PlacementDict(
+    "WindTunnel ($(direction)) (FactoryHelper)" => Ahorn.EntityPlacement(
+        WindTunnel,
+        "rectangle",
+        Dict{String, Any}(
+			"direction" => direction,
+		)
+    ) for direction in directions
+)
+
+function Ahorn.minimumSize(entity::WindTunnel)
+	return (16, 16)
+end
+
+Ahorn.nodeLimits(entity::WindTunnel) = 0, 0
+
+Ahorn.editingOptions(entity::WindTunnel) = Dict{String, Any}(
+    "direction" => directions
+)
+Ahorn.resizable(entity::WindTunnel) = true, true
+
+function Ahorn.renderAbs(ctx::Ahorn.Cairo.CairoContext, entity::WindTunnel, room::Maple.Room)
+	x, y = Ahorn.position(entity)
+	width = get(entity.data, "width", 16)
+	height = get(entity.data, "height", 16)
+	
+	Ahorn.drawRectangle(ctx, x, y, width, height, (0.7, 0.7, 0.7, 0.4), (0.7, 0.7, 0.7, 1.0))
+	
+	xf = xt = x
+	yf = yt = y
+	
+	direction = get(entity.data, "direction", "Up")
+	
+	if direction == "Up" || direction == "Down"
+		xf = xt = x + width/2
+	elseif direction == "Left" || direction == "Right"
+		yf = yt = y + height/2
+	end
+	if direction == "Up"
+		yf = y + height
+		yt = y
+	elseif direction == "Down"
+		yf = y
+		yt = y + height
+	elseif direction == "Left"
+		xf = x + width
+		xt = x
+	elseif direction == "Right"
+		xf = x
+		xt = x + width
+	end
+	
+	Ahorn.drawArrow(ctx, xf, yf, xt, yt, (0.0, 0.0, 0.7, 1.0), headLength=4)
+end
+
+function Ahorn.renderSelectedAbs(ctx::Ahorn.Cairo.CairoContext, entity::WindTunnel)
+end
+
+function Ahorn.selection(entity::WindTunnel)
+    x, y = Ahorn.position(entity)
+	width = get(entity.data, "width", 16)
+	height = get(entity.data, "height", 16)
+	
+    return Ahorn.Rectangle[Ahorn.Rectangle(x,y,width,height)]
+end
+
+end
