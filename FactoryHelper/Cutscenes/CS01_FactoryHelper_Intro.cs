11bbe93c8ec3886095cc7cfaa408bcad4a508bb7
acsBenceTamas <acs.bence.tamas@gmail.com>
2019-10-23 23:28:58 +0200

    Intro Cutscene

---
+++ FactoryHelper/Cutscenes/CS01_FactoryHelper_Intro.cs
@@ -0,0 +1,52 @@
+﻿using Celeste;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryHelper.Cutscenes
+{
+    class CS01_FactoryHelper_Intro : CutsceneEntity
+    {
+        public CS01_FactoryHelper_Intro()
+        {
+            Depth = Depths.Top*2;
+        }
+
+        public override void OnBegin(Level level)
+        {
+            Add(new Coroutine(Cutscene(level)));
+        }
+
+        private IEnumerator Cutscene(Level level)
+        {
+            yield return 2f;
+            yield return Textbox.Say("KaydenFox_FactoryMod_1_Factory_A_Intro");
+            yield return 2f;
+            EndCutscene(level);
+        }
+
+        public override void OnEnd(Level level)
+        {
+            level.OnEndOfFrame += delegate
+            {
+                level.UnloadLevel();
+                level.CanRetry = true;
+                level.Session.Level = "1-01-exterior";
+                level.Session.RespawnPoint = level.GetSpawnPoint(new Vector2(level.Bounds.Left, level.Bounds.Top));
+                level.LoadLevel(Player.IntroTypes.WalkInRight);
+            };
+        }
+
+        public override void Render()
+        {
+            Level level = Scene as Level;
+            Draw.Rect(color: Color.Black, x: level.Camera.X - 5f, y: level.Camera.Y - 5f, width: 370f, height: 190f);
+            base.Render();
+        }
+    }
+}
---
+++ FactoryHelper/Triggers/FactoryEventTrigger.cs
@@ -0,0 +1,28 @@
+﻿using Celeste;
+using Celeste.Mod.Entities;
+using FactoryHelper.Cutscenes;
+using Microsoft.Xna.Framework;
+using Monocle;
+
+namespace FactoryHelper.Triggers
+{
+    [CustomEntity("FactoryHelper/EventTrigger")]
+    class FactoryEventTrigger : Trigger
+    {
+        private string _eventName;
+
+        public FactoryEventTrigger(EntityData data, Vector2 offset) : base(data, offset)
+        {
+            _eventName = data.Attr("event");
+        }
+
+        public override void Awake(Scene scene)
+        {
+            base.Awake(scene);
+            if (_eventName == "factory_phone_intro")
+            {
+                Scene.Add(new CS01_FactoryHelper_Intro());
+            }
+        }
+    }
+}
