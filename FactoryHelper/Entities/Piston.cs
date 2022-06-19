a78b507992c637b025a599379d1d89f21dee985b
amctyg <amctyg@umsystem.edu>
2022-06-19 16:49:37 -0700

    (Piston.cs) fix lift speed with changing delta time

--- FactoryHelper/Entities/Piston.cs
+++ FactoryHelper/Entities/Piston.cs
@@ -563,7 +563,7 @@ namespace FactoryHelper.Entities
                         if (player != null)
                         {
                             float newY = _base.Y + _grabPositionModifier + (player.Y - (_base.Y + _grabPositionModifier)) * heightAfter / heightBefore;
-                            player.LiftSpeed = new Vector2(player.LiftSpeed.X, (newY - player.Y) / Engine.DeltaTime);
+                            player.LiftSpeed = new Vector2(player.LiftSpeed.X, (newY - player.Y) * Engine.DeltaTime);
                             player.MoveV(newY - player.Y);
                         }
                     }
@@ -588,7 +588,7 @@ namespace FactoryHelper.Entities
                         if (player != null)
                         {
                             float newX = _base.X + _grabPositionModifier + (player.X - (_base.X + _grabPositionModifier)) * widthAfter / widthBefore;
-                            player.LiftSpeed = new Vector2((newX - player.X) / Engine.DeltaTime, player.LiftSpeed.Y);
+                            player.LiftSpeed = new Vector2((newX - player.X) * Engine.DeltaTime, player.LiftSpeed.Y);
                             player.MoveH(newX - player.X);
                         }
                     }
